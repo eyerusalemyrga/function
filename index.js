be49bf031bf7f2a77122661023c023913acf2f5f
Eyerusalem Yrga <eyerusalemykahsay>
2025-04-10 10:00:21 +0300

    added js file

---
+++ index.js
@@ -0,0 +1,36 @@
+function strlength(length){
+return length.map(string=>string.length)
+}
+const list=["eyeru","lwam","helen"];
+const count=strlength(list);
+console.log(count);
+function evenNumber(integer){
+    return integer.filter(int=>int%2==0);
+} 
+const eveninteger=evenNumber(numberArray);
+console.log(eveninteger);
+function object(score){
+    return score.filter(scores=>score>75).map(scores=>score.map);
+
+}
+const studentScore=[
+    {fullname:"eyeru",score:70},
+    {fullname:"milen", score:40}
+]
+const scoreOrder=object(object);
+console.log(scoreOrder);
+function originalarray(squarednum){
+    return squarednum.map(numeric=>numeric*numeric)
+}
+const array=[1,2,3];
+const oldarray=originalarray(array);
+console.log(oldarray);
+function lengtharray(array){
+    return array.filter(object=>typeofobject==="string").map[
+        string=>string=>string.toUpperCase()
+
+    ]
+}
+const oriarray=[2,5,"eyeru"];
+const string=lengtharray(oriarray);
+console.log(string);
